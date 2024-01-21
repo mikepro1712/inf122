11c71bbefe75eca5e3f99cad068e7abe2681e30a
mikepro1712 <mnogalesa@fcpn.edu.bo>
2024-01-21 15:32:36 -0400

    vamooo

--- tres-en-raya/src/App.js
+++ tres-en-raya/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import './App.css';
 import Tablero from './components/Tablero';
+import TableroB from './components/TableroB';
+import TableroC from './components/TableroC';
+import TableroD from './components/TableroD';
+import TableroE from './components/TableroE';
 import Juego from './components/Juego';
+import JuegoA from './components/JuegoA';
 
 function App() {
 
@@ -10,9 +15,13 @@ function App() {
   return (
     <div className="App">
 
-      {/* <Tablero /> */}
-      <Juego />
+      {/* <Tablero />
+      <TableroB />
+      <TableroE />
+      <Juego />*/}
+      <JuegoA />
     </div>
   );
 }
+
 export default App;
